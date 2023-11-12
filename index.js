96e0629ec48652e9aa74dc72d8c2efd7fc5faab5
Ajay S <ajaycshan@gmail.com>
2023-11-12 21:43:40 +1100

    Add remaining choices in switch-case

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Main application file
 
 // Include packages needed for this application
-const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
@@ -11,14 +10,6 @@ const queries = require('./utils/queries.js')
 const sequelize = require('./config/connection.js');
 const consoleTable = require('console.table');
 
-
-
-function initialQuestion() {
-    inquirer.prompt({
-        type: 'list',
-
-    })
-}
 
 // Required:
 // viewAllDepartments
@@ -67,15 +58,19 @@ async function init() {
         case 'View all employees':
             await viewAllEmployees();
             break;
-
+        case 'Add a department':
+            await addDepartment();
+            break;
+        case 'Add a role':
+            await addRole();
+            break;
+        case 'Add an employee':
+            await addRole();
+            break;
+        case 'Update an employee role':
+            await addRole();
+            break;
     }
-}
-
-const init = () => {
-    inquirer.prompt(questions)
-    .then((data => writeToFile('./logo.svg', generateSVG(data))))
-    .then(() => console.log('Generated logo.svg!'))
-    .catch((err) => console.error(err));
 }
 
 // Function call to initialize app
